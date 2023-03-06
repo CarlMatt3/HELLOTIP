7460e47a47ebc018e72377fe9258d99cf7c71262
caraigii <qljccaraig@tip.edu.ph>
2023-03-07 00:50:29 +0800

    no func settings + no design

--- Assets/Scripts/SliderMenuAnim.cs
+++ Assets/Scripts/SliderMenuAnim.cs
@@ -1,34 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class SliderMenuAnim : MonoBehaviour
-// {
-//     public GameObject PanelMenu;
-//     public TourManager tourManager; // reference to the TourManager script
-
-//     public void ShowHideMenu()
-//     {
-//         if (PanelMenu != null)
-//         {
-//             Animator animator = PanelMenu.GetComponent<Animator>();
-//             if (animator != null)
-//             {
-//                 bool isOpen = animator.GetBool("show");
-//                 animator.SetBool("show", !isOpen);
-//                 if (!isOpen) // if the menu is being opened
-//                 {
-//                     tourManager.isCameraMove = false; // set isCameraMove to false
-//                 }
-//                 else // if the menu is being closed
-//                 {
-//                     tourManager.isCameraMove = true; // set isCameraMove to true
-//                 }
-//             }
-//         }
-//     }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
