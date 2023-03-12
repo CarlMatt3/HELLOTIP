4cacac5a85f3997d65f2d6a4c9dad6c6faeb9834
Sempai <juliusmjulian@gmail.com>
2023-03-12 08:17:13 +0800

    Code update

--- Assets/Scripts/MapZoom.cs
+++ Assets/Scripts/MapZoom.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MapZoom : MonoBehaviour, IScrollHandler
+public class MapZoom : MonoBehaviour, IScrollHandler, IDragHandler
 {
-private Vector3 initialScale;
+    private Vector3 initialScale;
+    private float touchDelta;
 
     [SerializeField]
     private float zoomSpeed = 0.1f;
     [SerializeField]
     private float maxZoom = 10f;
+    [SerializeField]
+    private float touchSensitivity = 1f;
 
     private void Awake()
     {
@@ -20,6 +23,17 @@ private Vector3 initialScale;
         var delta = Vector3.one * (eventData.scrollDelta.y * zoomSpeed);
         var desiredScale = transform.localScale + delta;
 
+        desiredScale = ClampDesiredScale(desiredScale);
+
+        transform.localScale = desiredScale;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        touchDelta += eventData.delta.magnitude * touchSensitivity;
+        var delta = Vector3.one * touchDelta * zoomSpeed;
+
+        var desiredScale = transform.localScale + delta;
         desiredScale = ClampDesiredScale(desiredScale);
 
         transform.localScale = desiredScale;
